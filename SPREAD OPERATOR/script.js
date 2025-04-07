4768f4cf171360f1686de40d4567b8941f15d458
pretender-2905 <pretender2905@gmail.com>
2025-04-08 02:13:48 +0500

    ssd

--- SPREAD OPERATOR/script.js
+++ SPREAD OPERATOR/script.js
@@ -1,6 +1,6 @@
-// ------- SPREAD OPERATOR with (array) -------  
-let arr = [1,2,3,4,5]
-let arr1 = [6,7,8,9,10, ...arr]
+// ------- SPREAD OPERATOR with (array) -------
+let arr = [1, 2, 3, 4, 5];
+let arr1 = [6, 7, 8, 9, 10, ...arr]; // spreading arr inside arr1
+let merge = [...arr, ...arr1];      // merging both arrays into a new one
 
-let merge = [...arr,...arr1]
-console.log(arr1);
+console.log(arr1); // Should print: [6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
