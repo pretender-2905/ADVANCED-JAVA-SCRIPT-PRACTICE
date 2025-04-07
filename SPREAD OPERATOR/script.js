303906a8887673d15f6499f4bab7fc274f66f176
pretender-2905 <pretender2905@gmail.com>
2025-04-08 02:46:56 +0500

    rrr

--- SPREAD OPERATOR/script.js
+++ SPREAD OPERATOR/script.js
@@ -1,6 +1,67 @@
-// ------- SPREAD OPERATOR with (array) -------
-let arr = [1, 2, 3, 4, 5];
-let arr1 = [6, 7, 8, 9, 10, ...arr]; // spreading arr inside arr1
-let merge = [...arr, ...arr1];      // merging both arrays into a new one
+// The spread operator in JavaScript is represented by three consecutive dots (...). It is used to unpack elements from an array or object and spread them out into a new array or object. 
 
-console.log(arr1); // Should print: [6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
+
+// ------- SPREAD OPERATOR with (array) -------
+// let arr = [1, 2, 3, 4, 5];
+// let arr1 = [6, 7, 8, 9, 10, ...arr]; // spreading arr inside arr1
+// let merge = [...arr, ...arr1];      // merging both arrays into a new one
+
+// console.log(arr1); // Should print: [6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
+// console.log(merge)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SPREAD OPERATOR WITH ARRAY
+
+let arrr1 = [1,2,3,4,5,6];
+let arrr2 = [7,8,9,10,11];
+let merge_arr = [...arrr1, ...arrr2];
+console.log(merge_arr);
+
+// spread operator woth objects
+
+
+let obj1 = {
+    name: "ibrahim",
+    age: 19
+}
+let obj2 = {
+    city: "karachi",
+    uni: "SMIU"
+}
+
+let merge_obj = {...obj1, ...obj2}
+console.log(merge_obj)
+
+
